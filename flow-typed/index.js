8274f36d74e06b855b01f9a282725c54a96c2409
koto hajime <toxictoxer@gmail.com>
2017-09-26 16:51:57 +0900

    Publish chenv (#1)
    * init chenv
    
    * Update .travis.yml
    
    * Modify .gitignore to remove flow-typed/
    
    * Add test/withoutManifest
    
    * Modiry test/index.js about err.message
    
    * Modify .travis.yml after_success
    
    * Add README.md and program.on(--help) for real
    
    * Update README.md
    
    * Update package.json

---
+++ flow-typed/index.js
@@ -0,0 +1,7 @@
+// declare module 'dtz' {
+//   declare module.exports: any
+// }
+
+// declare module 'fs-extra' {
+//   declare module.exports: any
+// }
---
+++ src/manifest.js
@@ -0,0 +1,11 @@
+export const deleteManifest = {
+   manifest_version: 2,
+   name: '(deleted)',
+   version: '0'
+}
+
+export const createManifest = {
+   manifest_version: 2,
+   name: '',
+   version: '0.0.0'
+}
