149dd76e44490d3f1a0e9809343af142dea56222
kthjm <toxictoxer@gmail.com>
2018-12-13 15:35:53 +0900

    Rename config.alias to config.items

--- bin/actions.js
+++ bin/actions.js
@@ -3,7 +3,7 @@ import opn from 'opn'
 import { loadCredentials, loadConfig } from './load'
 import { Chenv, authURL, getRefreshToken } from '..'
 
-const frame = (action) => 
+const action = (action) => 
   Promise.resolve()
   .then(action)
   .catch((err) => {
@@ -11,34 +11,24 @@ const frame = (action) =>
     process.exit(1)
   })
 
-const alias2items = (aliasName, map) =>
-  !aliasName
-  ?
-  []
-  :
-  aliasName
-  .split(',')
-  .filter(name => name)
-  .map(name => ({ name, ...map[name] }))
-
-const map2items = (map) =>
-  Object
-  .entries(map)
-  .map(([ name, item ]) => ({ name, ...item }))
-
-const aliasItems = (map, all, aliasName) =>
-  all ? map2items(map) : alias2items(aliasName, map)
-
 const promiseOrder = (args, action, index = 0) =>
-  !args[index]
-  ?
-  false
-  :
+  args[index] &&
   Promise.resolve()
   .then(() => action(args[index], index, args))
   .then(() => promiseOrder(args, action, index + 1))
-  
-export const auth = ({ app, env }) => frame(async () => {
+
+const order = (items, action) =>
+  !items.length
+  ?
+  console.log('\n > no item\n')
+  :
+  promiseOrder(items, async (item, index, items) => {
+    console.log(item)
+    await action(item, index, items)
+    return index !== items.length - 1 && console.log('-')
+  })
+
+export const auth = ({ app, env }) => action(async () => {
   const { client_id, client_secret } = loadCredentials(env)
 
   if (!client_id || !client_secret) {
@@ -60,7 +50,7 @@ export const auth = ({ app, env }) => frame(async () => {
   return console.log(`\n > REFRESH_TOKEN=${refresh_token}\n`)
 })
   
-export const upload = (src, id, options) => frame(() => {
+export const upload = (src, id, options) => action(() => {
   const {
     aliasName,
     all,
@@ -73,29 +63,26 @@ export const upload = (src, id, options) => frame(() => {
   const chenv = new Chenv(loadCredentials(env))
   
   const {
-    alias: aliasMap = {}
+    items: itemMap = {}
   } = loadConfig(config)
   
   const items = [
     { src, id },
-    ...aliasItems(aliasMap, all, aliasName)
+    ...aliasItems(itemMap, all, aliasName)
   ].filter(({ src }) => src)
   
-  return !items.length
-  ? console.log('\n > no item\n')
-  : promiseOrder(items, ({ name, src, id }) => {
-    console.log({ name, src, id })
-    return !id
+  return order(items, ({ name, src, id }) =>
+    !id
     ? chenv.insertItem(src).then(console.log)
     : chenv.updateItem(id, src).then(console.log).then(() =>
       publish ? chenv.publishItem(id, false).then(console.log) :
       publishTt ? chenv.publishItem(id, true).then(console.log) :
       false
     )
-  })
+  )
 })
 
-export const remove = (id, options) => frame(() => {
+export const remove = (id, options) => action(() => {
   const {
     aliasName,
     all,
@@ -106,23 +93,20 @@ export const remove = (id, options) => frame(() => {
   const chenv = new Chenv(loadCredentials(env))
   
   const {
-    alias: aliasMap = {}
+    items: itemMap = {}
   } = loadConfig(config)
   
   const items = [
     { id },
-    ...aliasItems(aliasMap, all, aliasName)
+    ...aliasItems(itemMap, all, aliasName)
   ].filter(({ id }) => id)
   
-  return !items.length
-  ? console.log('\n > no item\n')
-  : promiseOrder(items, ({ name, id }) => {
-    console.log({ name, id })
-    return chenv.removeItem(id).then(console.log)
-  })
+  return order(items, ({ name, id }) =>
+    chenv.removeItem(id).then(console.log)
+  )
 })
 
-export const check = (id, options) => frame(() => {
+export const check = (id, options) => action(() => {
   const {
     aliasName,
     all,
@@ -135,18 +119,35 @@ export const check = (id, options) => frame(() => {
   const chenv = new Chenv(loadCredentials(env))
   
   const {
-    alias: aliasMap = {}
+    items: itemMap = {}
   } = loadConfig(config)
   
   const items = [
     { id, projection: draft ? 'DRAFT' : published ? 'PUBLISHED' : '' },
-    ...aliasItems(aliasMap, all, aliasName)
+    ...aliasItems(itemMap, all, aliasName)
   ].filter(({ id }) => id)
   
-  return !items.length
-  ? console.log('\n > no item\n')
-  : promiseOrder(items, ({ name, id, projection }) => {
-    console.log({ name, id, projection })
-    return chenv.checkItem(id, projection).then(console.log)
-  })
+  return order(items, ({ name, id, projection }) =>
+    chenv.checkItem(id, projection).then(console.log)
+  )
 })
+
+const aliasItems = (map, all, aliasName) =>
+  all
+  ? map2items(map)
+  : alias2items(aliasName, map)
+
+const map2items = (map) =>
+  Object
+  .entries(map)
+  .map(([ name, item ]) => ({ name, ...item }))
+
+const alias2items = (aliasName, map) =>
+  !aliasName
+  ?
+  []
+  :
+  aliasName
+  .split(',')
+  .filter(name => name)
+  .map(name => ({ name, ...map[name] }))
